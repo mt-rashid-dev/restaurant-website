f981840bb8f353ad588257ac286bd6cec7a54e58
devCake1 <shipc4836@gmail.com>
2025-08-07 15:14:57 +0600

    add menu content to Home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,15 @@
 import "./Home.css";
 import foodImage1 from "../../assets/food-image-1.jpg";
 import chefImage1 from "../../assets/chef-image-1.jpg";
+import appetizer from "../../assets/appetizer.png";
+import dinner from "../../assets/dinner.png";
+import dessert from "../../assets/dessert.png";
+import coffeePot from "../../assets/coffee-pot.png";
 
 const Home = () => {
   return (
     <div>
+      {/* Top Banner */}
       <div className="Home-top-banner">
         {/* tb-content = top-banner-content */}
         <div className="Home-tb-content rounded-5">
@@ -46,6 +51,41 @@ const Home = () => {
               <div>
                 <h4><i className="bi bi-check-circle Home-check-icon"></i>&nbsp;<span className="color-gray-1 fw-bold">Easy Reservation</span></h4>
                 <p className="color-gray-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates non sapiente nam est doloremque ducimus obcaecati.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Menu Content */}
+      <div className="Home-menu-content py-5">
+        <div className="container">
+          <h1 className="text-center fw-bold mb-3 py-2 rounded-pill">Menu</h1>
+          <div className="row">
+            <div className="col-12 col-md-6 col-lg-3">
+              {/* mc = menu category */}
+              <div className="Home-mc m-2 p-3 rounded-5">
+                {/* mci = menu category icon */}
+                <img src={appetizer} alt="" className="Home-mci d-block mx-auto p-2 rounded-circle"/>
+                <h4 className="fw-bold mt-3 text-center">Appetizers</h4>
+              </div>
+            </div>
+            <div className="col-12 col-md-6 col-lg-3">
+              <div className="Home-mc m-2 p-3 rounded-5">
+                <img src={dinner} alt="" className="Home-mci d-block mx-auto p-2 rounded-circle"/>
+                <h4 className="fw-bold mt-3 text-center">Entrees</h4>
+              </div>
+            </div>
+            <div className="col-12 col-md-6 col-lg-3">
+              <div className="Home-mc m-2 p-3 rounded-5">
+                <img src={dessert} alt="" className="Home-mci d-block mx-auto p-2 rounded-circle"/>
+                <h4 className="fw-bold mt-3 text-center">Desserts</h4>
+              </div>
+            </div>
+            <div className="col-12 col-md-6 col-lg-3">
+              <div className="Home-mc m-2 p-3 rounded-5">
+                <img src={coffeePot} alt="" className="Home-mci d-block mx-auto p-2 rounded-circle"/>
+                <h4 className="fw-bold mt-3 text-center">Tea & Coffee</h4>
               </div>
             </div>
           </div>
